b869e78614552d0dde62a62d2d0038e7daeec59b
Victoria Helin <vickanhelin@hotmail.com>
2026-04-03 14:16:14 +0200

    lagt till variabler för de olika input-elementen

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,20 @@ interface courses {
     syllabus: string;
 }
 
+// Formulär 
 const form = document.querySelector<HTMLInputElement>("form")!;
+//Kurkod input
+const codeInput = document.getElementById("courseCode") as HTMLInputElement;
+//Kursnamn input
+const nameInput = document.getElementById("courseName") as HTMLInputElement;
+//Progression input
+const progInput = document.getElementById("progression") as HTMLInputElement;
+//Syllabus Input
+const linkInput = document.getElementById("syllabus") as HTMLInputElement;
 
 form.addEventListener("submit", (event: SubmitEvent) => {
     event.preventDefault();
     console.log("Du klickade");
+    console.log(codeInput.value, nameInput.value, progInput.value, linkInput.value);
+    
 });
