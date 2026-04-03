53d78dbc0028d2d5c7c65f495bf16fefb2ea650a
Victoria Helin <vickanhelin@hotmail.com>
2026-04-03 13:19:58 +0200

    lagt till interface för kurser, läst ut form-elementet och lagt till händelselyssnare på klick

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
 "use strict";
 
+interface courses {
+    code: string;
+    name: string;
+    progression: string;
+    syllabus: string;
+}
+
+const form = document.querySelector<HTMLInputElement>("form")!;
+
+form.addEventListener("submit", (event: SubmitEvent) => {
+    event.preventDefault();
+    console.log("Du klickade");
+});
