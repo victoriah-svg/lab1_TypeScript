f73aa87d7a0a5486143025d286b4ecbbd5a0dcc2
Victoria Helin <vickanhelin@hotmail.com>
2026-04-08 09:51:46 +0200

    lagt till loop för deleteknappar som anropar funktionen deleteCourses

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ interface course {
     syllabus: string;
 }
 
-  // Formulär 
+// Formulär 
 const form = document.querySelector<HTMLInputElement>("form")!;
 // Tabell
 const tableBody = document.querySelector("tbody")!;
@@ -28,74 +28,75 @@ let courseCodeArr: string[] = [];
 //Array för tillagda kurser 
 let courseArr: course[] = JSON.parse(localStorage.getItem("courses") || "[]");
 
-document.addEventListener("DOMContentLoaded", () =>{
-printCourses();
+document.addEventListener("DOMContentLoaded", () => {
+    printCourses();
 
-//Händelselyssnare på klick på sumbit formulär
-form.addEventListener("submit", (event: SubmitEvent) => {
-    event.preventDefault();
 
-    errors = [];
-    //tömmer div för felmeddelanden mellan varje klick
-    errorDiv.innerHTML = "";
+    //Händelselyssnare på klick på sumbit formulär
+    form.addEventListener("submit", (event: SubmitEvent) => {
+        event.preventDefault();
 
-    //Validering för ifyllnad som genererar felmeddelanden om ngt saknas
-    if (codeInput.value == "") {
-        errors.push("* Du måste fylla i korrekt kod");
-    }
+        errors = [];
+        //tömmer div för felmeddelanden mellan varje klick
+        errorDiv.innerHTML = "";
 
-    if (nameInput.value == "") {
-        errors.push("* Du måste fylla i ett kursnamn");
-    }
-
-    //Validerar att progression är A, B eller C
-    if (progInput.value == "" || (progInput.value !== "A" && progInput.value !== "B" && progInput.value !== "C")) {
-        errors.push("* Du måste fylla i A, B eller C som kursprogression");
-    }
-
-    if (linkInput.value == "") {
-        errors.push("* Du måste ange länk till kursplan");
-    }
-
-    //Validerar att kurskoden är unik
-    if (courseCodeArr.includes(codeInput.value)) {
-        errors.push("* Kurskoden du fyller i måste vara unik");
-    } else { courseCodeArr.push(codeInput.value); }
-
-    //Om array med felmeddelanden är tom, sparar ifyllda uppgifter i objekt och skickar med i anrop funktion printCourses 
-    if (errors.length === 0) {
-
-        //Ett objekt för ifylld kurs
-        const newCourse: course = {
-            code: codeInput.value,
-            name: nameInput.value,
-            progression: progInput.value,
-            syllabus: linkInput.value,
+        //Validering för ifyllnad som genererar felmeddelanden om ngt saknas
+        if (codeInput.value == "") {
+            errors.push("* Du måste fylla i korrekt kod");
         }
-        printCourses(newCourse);
 
-    }
+        if (nameInput.value == "") {
+            errors.push("* Du måste fylla i ett kursnamn");
+        }
 
-    /* Loopar igenom array för felmeddelanden och skriver ut i DOM */
-    errors.forEach(error => {
-        errorDiv.innerHTML += `
+        //Validerar att progression är A, B eller C
+        if (progInput.value == "" || (progInput.value !== "A" && progInput.value !== "B" && progInput.value !== "C")) {
+            errors.push("* Du måste fylla i A, B eller C som kursprogression");
+        }
+
+        if (linkInput.value == "") {
+            errors.push("* Du måste ange länk till kursplan");
+        }
+
+        //Validerar att kurskoden är unik
+        if (courseCodeArr.includes(codeInput.value)) {
+            errors.push("* Kurskoden du fyller i måste vara unik");
+        } else { courseCodeArr.push(codeInput.value); }
+
+        //Om array med felmeddelanden är tom, sparar ifyllda uppgifter i objekt och skickar med i anrop funktion printCourses 
+        if (errors.length === 0) {
+
+            //Ett objekt för ifylld kurs
+            const newCourse: course = {
+                code: codeInput.value,
+                name: nameInput.value,
+                progression: progInput.value,
+                syllabus: linkInput.value,
+            }
+            printCourses(newCourse);
+
+        }
+
+        /* Loopar igenom array för felmeddelanden och skriver ut i DOM */
+        errors.forEach(error => {
+            errorDiv.innerHTML += `
     <p>${error}</p>`;
-    })
+        })
 
-});
+    });
 })
 
 
 function printCourses(writtenCourse?: course): void {
     /*hämtar array från localStorage */
-   // let courseArr: course[] = JSON.parse(localStorage.getItem("courses") || "[]");
+    // let courseArr: course[] = JSON.parse(localStorage.getItem("courses") || "[]");
     //lägger till objektet för kurs i array
-   if (writtenCourse){
-    courseArr.push(writtenCourse);
-    //konverterar till sträng och sparar i localStorage
-    localStorage.setItem("courses", JSON.stringify(courseArr));
-   } 
-    
+    if (writtenCourse) {
+        courseArr.push(writtenCourse);
+        //konverterar till sträng och sparar i localStorage
+        localStorage.setItem("courses", JSON.stringify(courseArr));
+    }
+
 
     tableBody.innerHTML = "";
     //loopar igenom arrayen 
@@ -105,33 +106,28 @@ function printCourses(writtenCourse?: course): void {
             `<tr id=${course.code}>
             <td>${course.code}</td>
             <td>${course.name}</td>
-            <td>${course.syllabus}</td>
+            <td><a href=${course.syllabus}>länk</a></td>
             <td>${course.progression}</td>
-            <td><button id= ${course.code}>Ta bort</button></td>
+            <td><button class="deleteBtn" id= ${course.code}>Ta bort</button></td>
           </tr>`;
-        //funktion för att radera kurs
-        // console.log("Code: " + course.code, "Name: " + course.name, "progression :" + course.progression);
+        
     })
 
+    //deleteknappar
     
-    // console.log(courseArr);*/
-    /*
-     localStorage.setItem(`${writtenCourse.code}`, JSON.stringify(writtenCourse));
-     let storedCourse = localStorage.getItem(`${writtenCourse.code}`);
-  
-     if(storedCourse){
-      let parsedCourse = JSON.parse(storedCourse);
-       tableBody.innerHTML += 
-      `<tr id=${parsedCourse.code}>
-              <td>${parsedCourse.code}</td>
-              <td>${parsedCourse.name}</td>
-              <td>${parsedCourse.syllabus}</td>
-              <td>${parsedCourse.progression}</td>
-              <td><button>Ta bort</button></td>
-            </tr>`; 
-     } */
+    const deleteBtn = document.querySelectorAll(".deleteBtn");
 
+    if (deleteBtn !== null) {
+        deleteBtn.forEach(button => {
+            button.addEventListener("click", ()=>{
+                deleteCourses(button.id);
+            }); 
 
+        });
+    }
 
+}
 
+function deleteCourses(buttonid: string): void {
+    console.log("kurser raderas för knapp " + buttonid);
 }
